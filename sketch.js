23de2c0a98c0b10065efe91d1ba87026f026c396
lucask42 <lucasbkellner@gmail.com>
2017-04-20 00:36:12 -0700

    working countdown timer with URL param 'minutes'

--- sketch.js
+++ sketch.js
@@ -1,15 +1,35 @@
-let counter = 0
+function formatTime(s) {
+  let mins
+  let sec
+  mins = floor(s / 60)
+  sec = s % 60
+  return nf(mins,2) + ":" + nf(sec,2)
+}
 
 function setup(){
   noCanvas()
+  let timeLeft = 300
+  let params = getURLParams()
+  if(params.minute) {
+  let mins = params.minute;
+  timeLeft = mins * 60
+  }
+  let counter = 0
   let timer = select('#timer')
-  timer.html('0')
-
   function timeIt() {
     counter++
-    timer.html(counter)
+    timer.html(formatTime(timeLeft - counter))
+    if (counter == timeLeft){
+      ding.play()
+      counter = 0
+    }
   }
   setInterval(timeIt, 1000)
+}
+
+var ding
+function preload(){
+  //ding = loadSound("ding.mp3")
 }
 
 function draw(){
